e96de33d2e7a12614fdccf806e19540ad7cd3fc4
Yura Chornyi <chorny.yura@gmail.com>
2025-05-05 18:03:19 +0300

    feat: implement register endpoint

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -15,11 +15,58 @@ from database import (
     UserGroupEnum,
     ActivationTokenModel,
     PasswordResetTokenModel,
-    RefreshTokenModel
+    RefreshTokenModel,
 )
 from exceptions import BaseSecurityError
+from schemas import (
+    UserRegistrationResponseSchema,
+    UserRegistrationRequestSchema,
+)
 from security.interfaces import JWTAuthManagerInterface
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def register(
+    user_data: UserRegistrationRequestSchema,
+    db: AsyncSession = Depends(get_db),
+):
+    async with db as session:
+        user_exists_result = await session.execute(
+            select(UserModel).where(UserModel.email == user_data.email)
+        )
+        user_exists = user_exists_result.scalar_one_or_none()
+        if user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"A user with this email {user_data.email} already exists.",
+            )
+
+        try:
+            group_result = await session.execute(
+                select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER)
+            )
+            group = group_result.scalar_one_or_none()
+            new_user = UserModel.create(
+                email=user_data.email,
+                raw_password=user_data.password,
+                group_id=group.id,
+            )
+            session.add(new_user)
+            await session.flush()
+            await session.refresh(new_user)
+            activation_token = ActivationTokenModel(user=new_user)
+            session.add(activation_token)
+            await session.commit()
+            return new_user
+        except Exception:
+            await session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An error occurred during user creation.",
+            )
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,4 +3,31 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {
+        "from_attribute": True
+    }
+
+    @field_validator("email", mode="before")
+    @classmethod
+    def validate_email(cls, value: str):
+        return accounts_validators.validate_email(user_email=value)
+
+    @field_validator("password", mode="before")
+    @classmethod
+    def validate_password(cls, value: str):
+        return accounts_validators.validate_password_strength(password=value)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: str
+
+    model_config = {
+        "from_attribute": True
+    }
+
+
